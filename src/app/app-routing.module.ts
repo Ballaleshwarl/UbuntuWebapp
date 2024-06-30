d7be5b520a75720d58b0729027a6f23f8f01c4e6
ballaleshwar <bsg3415@gmail.com>
2024-07-01 00:02:45 +0530

    First Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,21 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 import { SignupComponent } from './signup/signup.component';
+import { InvestigateComponent } from './investigate/investigate.component';
+import { StudioComponent } from './studio/studio.component';
+import { PipelineComponent } from './pipeline/pipeline.component';
+import { RespondComponent } from './respond/respond.component';
+import { ConfigureComponent } from './configure/configure.component';
 
 const routes: Routes = [{path:'', component: LoginComponent, pathMatch:'full'},
   {path: 'home', component: HomeComponent},
-{path:'signup',component:SignupComponent}];
+{path:'signup',component:SignupComponent},
+{path:'investigate', component:InvestigateComponent},
+{path:'studio',component:StudioComponent},
+{path:'pipeline',component:PipelineComponent},
+{path:'respond',component:RespondComponent},
+{path:'configure',component:ConfigureComponent},
+{path:'**',redirectTo:''}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
